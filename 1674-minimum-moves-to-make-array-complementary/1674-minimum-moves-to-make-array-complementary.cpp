d0953f3d6b15608f2bce46b08f7ab9e2720735b5
RohanThakkar28 <115345904+RohanThakkar28@users.noreply.github.com>
2022-10-26 16:00:30 +0530

    Time: 1427 ms (5.75%), Space: 229.5 MB (8.05%) - LeetHub

---
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -0,0 +1,34 @@
+#define ll long long int
+class Solution {
+public:
+    int minMoves(vector<int>& a, int k) {
+        
+        vector<ll>pref(2*k+2,0);
+        ll n=a.size();
+        map<ll,ll>c;
+        for(int i=0;i<n/2;i++){
+            ll a1=a[i];
+            ll a2=a[n-i-1];
+            c[a1+a2]++;
+            ll l=min(a1,a2)+1;
+            ll r=max(a1,a2)+k;
+            pref[l]++;
+            pref[r+1]--;
+        }
+        for(int i=1;i<=2*k;i++){
+            pref[i]+=pref[i-1];
+
+        }
+        ll ans=n;
+        for(int i=2;i<=2*k;i++){
+            ll c0=c[i];
+            ll c1=pref[i]-c0;
+            ll c2=n/2-c0-c1;
+            ans=min(ans,c1+2*c2);
+
+        }
+        return ans;
+
+        
+    }
+};
