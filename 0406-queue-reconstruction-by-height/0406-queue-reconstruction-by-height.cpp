a83b510aa3e509748fa009f37d9f6e0478466ab1
RohanThakkar28 <115345904+RohanThakkar28@users.noreply.github.com>
2023-08-11 00:52:23 +0530

    Time: 23 ms (97.77%), Space: 13.5 MB (17.94%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -1,34 +1,68 @@
 class Solution {
 public:
+    int seg[4*100005];
+    void build(int id,int l,int r){
+        if(l==r){
+        //    cout<<id<<endl;
+            seg[id]=1;
+            return;
+        }
+        int m=l+(r-l)/2;
+        build(2*id+1,l,m);
+        build(2*id+2,m+1,r);
+        seg[id]=seg[2*id+1]+seg[2*id+2];
+    }
+    void update(int id){
+        while(id>0){
+            
+            seg[id]--;
+            id=(id-1)/2;
+        }
+    }
+    int query(int k,int id,int l,int r){
+        if(l==r){
+           update(id);
+            return l;
+        }
+        int lf=seg[2*id+1];
+        int m=l+(r-l)/2;
+        if(k<=lf){
+            return query(k,2*id+1,l,m);
+        }
+        else{
+            return query(k-lf,2*id+2,m+1,r);
+        }
+        
+    }
+    static bool sortcol(const vector<int>& v1, const vector<int>& v2)
+    {
+        if(v1[0]==v2[0]){
+            return v1[1]>v2[1];
+            
+        }
+        return v1[0] < v2[0];
+    }
+    
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(),people.end());
+        sort(people.begin(),people.end(),sortcol);
+        
         int n=people.size();
+        // for(int i=0;i<n;i++){
+        //     cout<<people[i][0]<<" "<<people[i][1]<<endl;
+        // }
+        build(0,0,n-1);
+     //   cout<<"hi"<<endl;
         vector<vector<int>>ans(n);
+        
         for(int i=0;i<n;i++){
-            int pos=0;
-            int rem=people[i][1];
-           
-            pos=rem;
-            int j=0;
-
-            while(j<n-1 && rem>0){
-                if(ans[j].size()==0 ){
-                    rem--;
-                }
-                else if(ans[j][0]==people[i][0]){
-                    rem--;
-                }
-                j++;
-            }
-            while(j<n-1 && ans[j].size()>0){
-                j++;
-            }
-            
-         //   cout<<people[i][0]<<" "<<people[i][1]<<" "<<j<<endl;
-                
-                
-            
-            ans[j]=people[i];
+          //  cout<<i<<" "<<pendl;
+            // for(int j=0;j<12;j++){
+            //     cout<<seg[j]<<" ";
+            // }
+            // cout<<endl;
+            int id=query(people[i][1]+1,0,0,n-1);
+           // cout<<id<<" "<<people[i][1]<<endl;
+            ans[id]=people[i];
         }
         return ans;
         
